e8e45ce43d565d83edbecd33fc414e90cc02ccbf
RICARDO DA SILVA ZANATA <ricardoszanata@gmail.com>
2023-02-17 11:56:52 -0300

    finalização da modelagem do banco de dados

--- script do banco.sql
+++ script do banco.sql
@@ -16,8 +16,28 @@ create table colaboradores(
     colcelular varchar(20),
     colemail varchar(100),
     coltipid int,
-    colativo boolean default 1
+    colativo boolean default 1,
+	constraint fkcoltipid foreign key(coltipid) references tiposdecoladoradores(tipid)
     );
+create table usuarios(
+	usuid int primary key auto_increment,
+	usucolid int,
+	ususetid int,
+	usulogin varchar(50),
+	usupass varchar(50),
+	usuativo boolean default 1,
+	constraint fkusucolid foreign key(usucolid) references colaboradores(colid),
+	constraint fkususetid foreign key(ususetid) references setores(setid)
+	);
 create table chamados(
 	chaid int primary key auto_increment,
 	chadataabertura timestamp default current_timestamp,
+	chadataprevisao timestamp,
+	chadatafinalizacao timestamp,
+	charespcolid int,
+	chasolicolid int,
+	chaexeccolid int,
+	chaproblema varchar(1024),
+	chasolucao varchar(1024),
+	chastatus enum('ABERTO','PENDENTE','CANCELADO','FECHADO')
+	);
